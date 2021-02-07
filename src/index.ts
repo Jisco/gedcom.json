83dd6e2dfe3cb763dca592dfce4e93114e3df0f5
Jens <JensHoernecke@gmx.de>
2021-02-07 11:14:17 +0100

    Add build script

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-#!/usr/bin/env node
-import yargs from 'yargs';
-import { Convert as ConvertToJSON } from "./ToJSON/console";
+import JsonParsing from "./ToJSON/models/Parsing";
+import ParsingOptions from "./ToJSON/models/ParsingOptions";
 
-const { hideBin } = require('yargs/helpers')
-let argv = yargs(hideBin(process.argv)).argv
-
-if (!argv.mode || argv.mode === "JSON") {
-    ConvertToJSON(argv);
-}
-else {
-    console.log("TODO: Conversion from JS\\JSON to GEDCOM");
+export {
+    JsonParsing,
+    ParsingOptions
 }
